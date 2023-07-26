48e2488d88cf32d32f8354817dc0ff9396c9f55b
Faizan Khokhar <muhammad.khokhar.97@gmail.com>
2023-07-27 00:03:40 +0500

    init controller

---
+++ src/main/java/com/example/controllers/HomeController.java
@@ -0,0 +1,14 @@
+package com.example.controllers;
+
+import java.security.Principal;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+    @GetMapping
+    public String home(Principal principal) {
+        return "Hello, " + principal.getName();
+    }
+}
