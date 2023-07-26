9eb4af457e55a87e8cf10f2013cc395b17b44407
Faizan Khokhar <muhammad.khokhar.97@gmail.com>
2023-07-26 23:59:02 +0500

    init java version

--- src/main/java/com/example/springjwt/SpringjwtApplication.java
+++ src/main/java/com/example/springjwt/SpringjwtApplication.java
@@ -1,13 +1,13 @@
-package com.example.demo;
+package com.example.springjwt;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
+public class SpringjwtApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(SpringjwtApplication.class, args);
 	}
 
 }
--- src/test/java/com/example/springjwt/SpringjwtApplicationTests.java
+++ src/test/java/com/example/springjwt/SpringjwtApplicationTests.java
@@ -1,10 +1,10 @@
-package com.example.demo;
+package com.example.springjwt;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class DemoApplicationTests {
+class SpringjwtApplicationTests {
 
 	@Test
 	void contextLoads() {
